f5323947d555db6a5980a650155f45e9bde77e7f
marissag <marissag@iastate.edu>
2020-09-02 13:02:30 -0500

    Backend side of Weather App Experiment

--- Experiments/Marissa/WeatherApp/app/src/main/java/com/example/weatherapp/Model/Coord.java
+++ Experiments/Marissa/WeatherApp/app/src/main/java/com/example/weatherapp/Model/Coord.java
@@ -1,4 +1,27 @@
 package com.example.weatherapp.Model;
 
 public class Coord {
+    private double lat;
+    private double lon;
+
+    public Coord(double lat, double lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat(){
+        return lat;
+    }
+
+    public double getLong(){
+        return lon;
+    }
+
+    public void setLat(double lat){
+        this.lat = lat;
+    }
+
+    public void setLon(double lon){
+        this.lon = lon;
+    }
 }
--- Experiments/Marissa/WeatherApp/app/src/main/java/com/example/weatherapp/Model/Main.java
+++ Experiments/Marissa/WeatherApp/app/src/main/java/com/example/weatherapp/Model/Main.java
@@ -1,4 +1,77 @@
 package com.example.weatherapp.Model;
 
 public class Main {
+    private double temp;
+    private double pressure;
+    private int humidity;
+    private double temp_min;
+    private double temp_max;
+    private double sea_level;
+    private double ground_level;
+
+    public Main(double temp, double pressure, int humidity, double temp_min, double temp_max, double sea_level, double ground_level) {
+        this.temp = temp;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.temp_min = temp_min;
+        this.temp_max = temp_max;
+        this.sea_level = sea_level;
+        this.ground_level = ground_level;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public double getTemp_min() {
+        return temp_min;
+    }
+
+    public double getTemp_max() {
+        return temp_max;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setTemp_min(double temp_min) {
+        this.temp_min = temp_min;
+    }
+
+    public void setTemp_max(double temp_max) {
+        this.temp_max = temp_max;
+    }
+
+    public double getSea_level() {
+        return sea_level;
+    }
+
+    public void setSea_level(double sea_level) {
+        this.sea_level = sea_level;
+    }
+
+    public double getGround_level() {
+        return ground_level;
+    }
+
+    public void setGround_level(double ground_level) {
+        this.ground_level = ground_level;
+    }
 }
