530d9ba0187730314b13071da944c7ce1e076d15
marissag <marissag@iastate.edu>
2020-09-14 09:56:51 -0500

    Updated Weather Experiment

--- Experiments/Marissa/WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ Experiments/Marissa/WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.weatherapp.Model.OpenWeatherMap;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtTime, txtCelsius;
+    ImageView imageView;
+
+    LocationManager locationManager;
+    String provider;
+    static double lat, lng;
+    OpenWeatherMap openWeatherMap = new OpenWeatherMap();
+    int MY_PERMISSION = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Control
+        txtCity = (TextView) findViewById(R.id.txtCity);
+
+        //First, get coordinates
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COURSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESSS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+            }, MY_PERMISSION)
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if(location == null){
+            LOG.e("TAG", "No Location");
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
     }
 }
