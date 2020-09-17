a91292ec3915c1bf776357ae0f55071c063f61a4
Adrian <adrianv@iastate.edu>
2020-09-16 23:20:02 -0500

    commented out line 12

--- Backend/Adrian/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/Adrian/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/test")
+	//@GetMapping("/test")
 	public String test(){
 		return String.format("Test!");
 	}
