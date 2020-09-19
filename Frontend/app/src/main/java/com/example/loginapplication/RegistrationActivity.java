81b60aea57c81be989ebd5c21870ca3e03af042e
marissag <marissag@iastate.edu>
2020-09-18 20:42:26 -0500

    Working! Need to change to JsonRequest and connect to server

--- Frontend/app/src/main/java/com/example/loginapplication/RegistrationActivity.java
+++ Frontend/app/src/main/java/com/example/loginapplication/RegistrationActivity.java
@@ -2,12 +2,14 @@ package com.example.loginapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -49,29 +51,38 @@ public class RegistrationActivity extends AppCompatActivity {
                 String url = "http://google.com";
                 StringRequest sRequest = new StringRequest(
                         Request.Method.GET, url,
-                        new Response.Listener<String>(){
+                        new Response.Listener<String>() {
                             @Override
-                            public void onResponse(String response){
-                                Log.d(TAG, "Response is: "+ response);
-                              credentials = new Credentials(regUsername,regPassword);
-                              startActivity(new Intent(RegistrationActivity.this, HomePageActivity.class));
-                              Toast.makeText(RegistrationActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                            public void onResponse(String response) {
+                                System.out.println("Response is: " + response);
+                                Log.d(TAG, "Response is: " + response);
+                                credentials = new Credentials(regUsername, regPassword);
+                                startActivity(new Intent(RegistrationActivity.this, HomePageActivity.class));
+
+                                Toast.makeText(RegistrationActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
                             }
                         },
-                        new Response.ErrorListener(){
+                        new Response.ErrorListener() {
                             @Override
-                            public void onErrorResponse(VolleyError error){
+                            public void onErrorResponse(VolleyError error) {
                                 //Log that
-                                Log.d(TAG, "Error is: " + error.getMessage()+"");
+                                System.out.println("There was an error dog: " + error.getMessage() + "");
+                                Log.d(TAG, "Error is: " + error.getMessage() + "");
                             }
                         }
-                );}
-                AppController2.getInstance();
-                AppController2.getInstance(RegistrationActivity.this).addRequestQueue(sRequest);
-                AppController.getInstance().addRequestQueue(sRequest);
+                );
+                //AppController2.getInstance(sRequest).addRequestQueue();
+                Log.d(TAG, "Adding the following to Request Queue: " + sRequest);
+                System.out.println("Adding the following to Request Queue: " + sRequest);
+                AppController.getInstance().addToRequestQueue(sRequest);
+            }
+                //AppController2.getInstance(sRequest).addRequestQueue();
+                //AppController.getInstance().addRequestQueue(sRequest);
 
         });
 
+//        AppController2.getInstance(sRequest).addRequestQueue();
+//        AppController.getInstance().addToRequestQueue(sRequest);
 
     }
 
@@ -81,7 +92,6 @@ public class RegistrationActivity extends AppCompatActivity {
             Toast.makeText(this, "Please enter all the details, password should be at least 8 characters", Toast.LENGTH_SHORT).show();
             return false;
         }
-
         return true;
     }
 }
--- Frontend/app/src/main/java/com/example/loginapplication/net_utils/Const.java
+++ Frontend/app/src/main/java/com/example/loginapplication/net_utils/Const.java
@@ -8,5 +8,7 @@ public class Const {
     public static final String URL_STRING_REQ = "http://api.androidhive.info/volley/string_respsonse.html";
     public static final String URL_IMAGE = "http://api.androidhive.info/volley/volley-image.jpg";
 
+    //SERVER:
+    //http://coms-309-cy-04.cs.iastate.edu:8080/blah
     //When trying to connect to localhost through your phone/emulator, you need to use http://10.0.2.2 not localhost
 }
