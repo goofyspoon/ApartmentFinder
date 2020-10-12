97d320ace756792360b70f86acf53c7574890314
Adrian <adrianv@iastate.edu>
2020-10-11 22:19:00 -0500

    get by name works, others do not

--- Backend/Adrian/betterOne/src/main/java/myProject/ApartmentDB.java
+++ Backend/Adrian/betterOne/src/main/java/myProject/ApartmentDB.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface ApartmentDB extends JpaRepository<Apartments, Integer> {
-
+//needs more gets and a sort
     List<Apartments> findByApartmentId(int userId);
     List<Apartments> deleteByApartmentId(int userId);
     List<Apartments> findByApartmentName(String userName);
